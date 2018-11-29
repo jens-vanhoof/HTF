d6fdec52742d7b521233d7f68496383bd5403f6c
Stijn Geens <stijn.geens@outlook.be>
2018-11-29 11:02:43 +0100

    Some changes

--- htf/Controllers/ChallengeController.cs
+++ htf/Controllers/ChallengeController.cs
@@ -9,6 +9,56 @@ namespace htf.Controllers
 {
     public class ChallengeController : Controller
     {
+        public class InputValue
+        {
+            public string name { get; set; }
+            public string data { get; set; }
+        }
+
+        public class Question
+        {
+            public List<InputValue> inputValues { get; set; }
+        }
+
+        public class InputValue2
+        {
+            public string name { get; set; }
+            public string data { get; set; }
+        }
+
+        public class Question2
+        {
+            public List<InputValue2> inputValues { get; set; }
+        }
+
+        public class Value
+        {
+            public string name { get; set; }
+            public string data { get; set; }
+        }
+
+        public class Answer
+        {
+            public string challengeId { get; set; }
+            public List<Value> values { get; set; }
+        }
+
+        public class Example
+        {
+            public Question2 question { get; set; }
+            public Answer answer { get; set; }
+        }
+
+        public class RootObject
+        {
+            public string id { get; set; }
+            public string identifier { get; set; }
+            public string title { get; set; }
+            public string description { get; set; }
+            public Question question { get; set; }
+            public Example example { get; set; }
+        }
+
         private readonly IHttpClientFactory _clientFactory;
 
         public ChallengeController(IHttpClientFactory clientFactory)
@@ -16,10 +66,10 @@ namespace htf.Controllers
             _clientFactory = clientFactory;
         }
 
-        public async Task<String> GetOne()
+        public async Task<RootObject> GetOne()
         {
             var request = new HttpRequestMessage(HttpMethod.Get,
-                "htf2018.azurewebsites.net/challenges/593bc0a2e0dfdc53b239bc2a96ab0fd5");
+                "http://htf2018.azurewebsites.net/challenges/593bc0a2e0dfdc53b239bc2a96ab0fd5");
             request.Headers.Add("htf-identification", "ODc2ZjM2NjYtZGUyNy00ZDczLThkN2QtOTY4ZTA2NzY3MGMy");
 
             var client = _clientFactory.CreateClient();
@@ -29,13 +79,12 @@ namespace htf.Controllers
             if (response.IsSuccessStatusCode)
             {
 
-                var test = await response.Content.ReadAsAsync<String>();
-                test = "succes";
+                var test = await response.Content.ReadAsAsync<RootObject>();
                 return test;
             }
             else
             {
-                return "error";
+                return null;
             }
         }
 
@@ -43,7 +92,7 @@ namespace htf.Controllers
         {
             var test = GetOne();
             ViewData["test"] = test;
-            return View(ViewData);
+            return View(test);
         }
     }
 }
--- htf/Models/One.cs
+++ htf/Models/One.cs
@@ -7,5 +7,10 @@ namespace htf.Models
 {
     public class One
     {
+        public string id { get; set; }
+        public string identifier { get; set; }
+        public string title { get; set; }
+        public string description { get; set; }
+        
     }
 }
